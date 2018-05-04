775fe7f9d7ef514f2156b2e8159a8bdba477d628
Ronald Gerlach <wordsmith4u@hotmail.com>
2018-05-04 20:08:03 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,34 +4,15 @@ class Person
     @name = name
   end
 
-  def name
-    @name
-  end
-
   def job=(job)
     @job = job
+  end
+
+  def name
+    @name
   end
 
   def job
     @job
   end
-
 end
-
-# Notes and practice
-
-class Record
-
-  def genre=(records_genre)
-    @grenre = records_genre
-  end
-
-  def genre
-    @genre
-  end
-
-end
-
-beasty_boys = Record.new
-beasty_boys.genre = "Rap and R&B"
-beasty_boys.gerne 
